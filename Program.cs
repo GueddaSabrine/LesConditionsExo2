18e67c59346351c249e7b468105f554da592651c
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-16 15:30:37 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LesConditionsExo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine(" Vous avez " + age + " ans, vous êtes donc majeur.e.");
+            }
+            else
+            {
+                Console.WriteLine("Vous avez" + age + "ans, vous êtes donc mineur.e.");
+            }
+        }
+    }
+}
